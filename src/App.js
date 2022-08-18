0c620dd99686378905b8eb0c6191dcaf2da57104
Jacob Erickson <Jakerson8@gmail.com>
2022-08-17 18:57:26 -0600

    Modal finished with logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,96 @@
 import React, { useState } from "react";
 import "./App.css";
-import Square from "./Square";
+import WinnerModal from "./components/WinnerModal";
+import Square from "./components/Square"
 function App() {
   const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState(true);
+  const [winner, setWinner] = useState();
+
   const handleClick = () => {
     setSquares(["", "", "", "", "", "", "", "", ""])
     setPlayer(true)}
+  const newGameHandler = () =>{
+    setWinner(null)
+    setPlayer(true)
+  }
+
+ if((squares[0]==='X'&&squares[1]==='X'&&squares[2]==='X')||(squares[3]==='X'&&squares[4]==='X'&&squares[5]==='X')||
+ (squares[6]==='X'&&squares[7]==='X'&&squares[8]==='X')){
+  setWinner({title: "Hack Bright Wins",
+    message: "Wins by Horizontal",})
+
+    setTimeout(function() {
+    }, 1000);
+    handleClick()
+    return;
+ }
+
+ if((squares[0]==='O'&&squares[1]==='O'&&squares[2]==='O')||(squares[3]==='O'&&squares[4]==='O'&&squares[5]==='O')||
+ (squares[6]==='O'&&squares[7]==='O'&&squares[8]==='O')){
+  setWinner({title: "Dev-Mountain Wins",
+    message: "Wins by Horizontal",})
+
+    setTimeout(function() {
+    }, 1000);
+    handleClick()
+    return;
+ }
+
+ if((squares[0]==='X'&&squares[3]==='X'&&squares[6]==='X')||(squares[1]==='X'&&squares[4]==='X'&&squares[7]==='X')||
+ (squares[2]==='X'&&squares[5]==='X'&&squares[8]==='X')){
+  setWinner({title: "Hack Bright Wins",
+    message: "Wins by Vertical",})
+
+    setTimeout(function() {
+    }, 1000);
+    handleClick()
+    return;
+ }
+
+ if((squares[0]==='O'&&squares[3]==='O'&&squares[6]==='O')||(squares[1]==='O'&&squares[4]==='O'&&squares[7]==='O')||
+ (squares[2]==='O'&&squares[5]==='O'&&squares[8]==='O')){
+  setWinner({title: "Dev-Mountain Wins",
+    message: "Wins by Vertical",})
+
+    setTimeout(function() {
+    }, 1000);
+    handleClick()
+    return;
+ }
+
+ if((squares[0]==='X'&&squares[4]==='X'&&squares[8]==='X')){
+  setWinner({title: "Hack Bright Wins",
+    message: "Wins by Diagonal",})
+
+    setTimeout(function() {
+    }, 1000);
+    handleClick()
+    return;
+ }
+
+ if((squares[0]==='O'&&squares[4]==='O'&&squares[8]==='O')){
+  setWinner({title: "Dev-Mountain Wins",
+    message: "Wins by Diagonal",})
+
+    setTimeout(function() {
+    }, 1000);
+    handleClick()
+    return;
+ }
+
+
+
+
+
+
+
   return (
     <div className="App">
+      {winner &&<WinnerModal
+      onOkay = {newGameHandler} 
+      title={winner.title}
+      message={winner.message} />}
       <div className="container">
         {squares.map((val, index) => {
           return (
--- src/components/Square.js
+++ src/components/Square.js
@@ -13,13 +13,13 @@ const Square = (props) => {
         }
     }
 };
-console.log(props.squares, props.player)
 return(
+    <>
   <div className="square" onClick={handleClick}>
         {props.squareValue === "O" && <img src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" alt="O" />}
         {props.squareValue === "X" && <img src="https://ih1.redbubble.net/image.209567622.8660/st,small,507x507-pad,600x600,f8f8f8.u3.jpg"alt="X" />}
         </div>
-  
+    </>
     )
 };
 export default Square;
