d77a861f704cafb22815f0341d610547172c4165
Jacob Erickson <Jakerson8@gmail.com>
2022-08-17 23:38:34 -0600

    Player Header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import WinnerModal from "./components/WinnerModal";
 import Square from "./components/Square"
+import Button from "./UI/Button";
 function App() {
   const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState(true);
@@ -10,19 +11,19 @@ function App() {
   const handleClick = () => {
     setSquares(["", "", "", "", "", "", "", "", ""])
     setPlayer(true)}
+
+
   const newGameHandler = () =>{
     setWinner(null)
     setPlayer(true)
   }
 
- if((squares[0]==='X'&&squares[1]==='X'&&squares[2]==='X')||(squares[3]==='X'&&squares[4]==='X'&&squares[5]==='X')||
+ const checkForWinner=(squares)=>{ 
+  if((squares[0]==='X'&&squares[1]==='X'&&squares[2]==='X')||(squares[3]==='X'&&squares[4]==='X'&&squares[5]==='X')||
  (squares[6]==='X'&&squares[7]==='X'&&squares[8]==='X')){
   setWinner({title: "Hack Bright Wins",
     message: "Wins by Horizontal",})
-
-    setTimeout(function() {
-    }, 1000);
-    handleClick()
+    setSquares(["", "", "", "", "", "", "", "", ""])
     return;
  }
 
@@ -30,10 +31,7 @@ function App() {
  (squares[6]==='O'&&squares[7]==='O'&&squares[8]==='O')){
   setWinner({title: "Dev-Mountain Wins",
     message: "Wins by Horizontal",})
-
-    setTimeout(function() {
-    }, 1000);
-    handleClick()
+    setSquares(["", "", "", "", "", "", "", "", ""])
     return;
  }
 
@@ -41,10 +39,7 @@ function App() {
  (squares[2]==='X'&&squares[5]==='X'&&squares[8]==='X')){
   setWinner({title: "Hack Bright Wins",
     message: "Wins by Vertical",})
-
-    setTimeout(function() {
-    }, 1000);
-    handleClick()
+    setSquares(["", "", "", "", "", "", "", "", ""])
     return;
  }
 
@@ -52,45 +47,38 @@ function App() {
  (squares[2]==='O'&&squares[5]==='O'&&squares[8]==='O')){
   setWinner({title: "Dev-Mountain Wins",
     message: "Wins by Vertical",})
-
-    setTimeout(function() {
-    }, 1000);
-    handleClick()
+    setSquares(["", "", "", "", "", "", "", "", ""])
     return;
  }
 
- if((squares[0]==='X'&&squares[4]==='X'&&squares[8]==='X')){
+ if((squares[0]==='X'&&squares[4]==='X'&&squares[8]==='X')||(squares[2]==='X'&&squares[4]==='X'&&squares[6]==='X')){
   setWinner({title: "Hack Bright Wins",
     message: "Wins by Diagonal",})
-
-    setTimeout(function() {
-    }, 1000);
-    handleClick()
+    setSquares(["", "", "", "", "", "", "", "", ""])
     return;
  }
 
- if((squares[0]==='O'&&squares[4]==='O'&&squares[8]==='O')){
+ if((squares[0]==='O'&&squares[4]==='O'&&squares[8]==='O')||(squares[2]==='O'&&squares[4]==='O'&&squares[6]==='O')){
   setWinner({title: "Dev-Mountain Wins",
     message: "Wins by Diagonal",})
-
-    setTimeout(function() {
-    }, 1000);
-    handleClick()
+    setSquares(["", "", "", "", "", "", "", "", ""])
     return;
- }
+ }}
 
-
-
-
-
-
+ const getPlayerHandler = () =>{
+  
+}
 
   return (
     <div className="App">
+      <h1>TIC-TAC-TOE</h1>
+      {player && <h2 className="hack">HackBright its your turn!</h2>}
+      {!player && <h2 className="dev">DevMountain its your turn!</h2>}
       {winner &&<WinnerModal
       onOkay = {newGameHandler} 
       title={winner.title}
       message={winner.message} />}
+      <span>{checkForWinner(squares)}</span>
       <div className="container">
         {squares.map((val, index) => {
           return (
@@ -105,7 +93,7 @@ function App() {
           );
         })}
       </div>
-      <button onClick={handleClick}>Reset</button>
+      <Button onClick={handleClick}>Reset</Button>
     </div>
   );
 }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,5 +1,7 @@
 import React from "react";
 const Square = (props) => {
+
+
   const handleClick = () => {
     if (!props.squareValue) {
         if (props.player) {
